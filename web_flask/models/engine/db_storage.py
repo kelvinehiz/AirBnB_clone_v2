d00e594088dab92e1ab47336dedd7d8582bb987f
kelvinehiz <kelvinehiz@gmail.com>
2024-04-23 21:05:18 +0000

    removed white spaces

--- web_flask/models/engine/db_storage.py
+++ web_flask/models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
+# new class for sqlAlchemy 
 from os import getenv
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
@@ -14,7 +14,7 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """ create tables in environmental"""
+    # create tables in environmental
     __engine = None
     __session = None
 
@@ -33,10 +33,10 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
+        #returns a dictionary
         Return:
             returns a dictionary of __object
-        """
+        
         dic = {}
         if cls:
             if type(cls) is str:
@@ -55,30 +55,28 @@ class DBStorage:
         return (dic)
 
     def new(self, obj):
-        """add a new element in the table
-        """
+        #add a new element in the table
         self.__session.add(obj)
 
     def save(self):
-        """save changes
-        """
+        #save changes
+       
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete an element in the table
-        """
+        #delete an element in the table
         if obj:
             self.session.delete(obj)
 
     def reload(self):
-        """configuration
-        """
+        #configuration
+        
         Base.metadata.create_all(self.__engine)
         sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sec)
         self.__session = Session()
 
     def close(self):
-        """ calls remove()
-        """
+        # calls remove()
+        
         self.__session.close()
